5d8702313e421a1f059b0b3ebc7bdeab0e4b5014
Dark_Han <darhan.bukenov97@gmail.com>
2020-02-19 17:09:35 +0600

    Install logo and Create Menu

--- app/src/main/java/com/example/mydiploma/MainActivity.java
+++ app/src/main/java/com/example/mydiploma/MainActivity.java
@@ -2,11 +2,19 @@ package com.example.mydiploma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button button1;
+    Button button2;
+    Button button3;
 
     BottomNavigationView bottomNavigationView;
 
@@ -15,6 +23,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.navView);
+
+        button1 = (Button) findViewById(R.id.buttonSpine);
+        button2 = (Button) findViewById(R.id.buttonHand);
+        button3 = (Button) findViewById(R.id.buttonFoot);
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.buttonSpine:
+                startActivity(new Intent(MainActivity.this, SpineActivity.class));
+                Animatoo.animateSlideLeft(MainActivity.this);
+                break;
+            case R.id.buttonHand:
+                startActivity(new Intent(MainActivity.this, HandActivity.class));
+                Animatoo.animateSlideLeft(MainActivity.this);
+                break;
+            case R.id.buttonFoot:
+                startActivity(new Intent(MainActivity.this, FootActivity.class));
+                Animatoo.animateSlideLeft(MainActivity.this);
+                break;
+        }
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
 
     }
 }
---
+++ app/src/main/java/com/example/mydiploma/SpineActivity.java
@@ -0,0 +1,14 @@
+package com.example.mydiploma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spine);
+    }
+}
